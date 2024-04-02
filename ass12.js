2cf52e6428f13be2ac2351682ae281c20a732ec2
RestorerBarisereden <restorerzorzor@gmail.com>
2024-04-02 23:49:43 +0100

    first push

---
+++ ass12.js
@@ -0,0 +1,13 @@
+let age = 22
+if ("age <= 18"){
+    console.log("Basic Subscribers");
+}
+else if ("age >= 18 && preference = Sport"){
+    console.log("Premium Sports");
+}
+else if ("age >= 18 && preference = Not specific"){
+    console.log("Standard package");
+}
+else{
+    console.log("subscription invalid");
+}
